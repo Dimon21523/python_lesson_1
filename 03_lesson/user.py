9b00a7d266b1bb9eeab5099810d5aae8bb1911ff
Dimon21523 <dimitrisapollon041@gmail.com>
2025-11-17 22:03:10 +0200

    Fix flake8 warning

--- 03_lesson/user.py
+++ 03_lesson/user.py
@@ -3,11 +3,11 @@ class User:
         self.first_name = first_name
         self.last_name = last_name
 
-    def print_first_name (self):
+    def print_first_name(self):
         print(self.first_name)
 
-    def print_last_name (self):
+    def print_last_name(self):
         print(self.last_name)
 
-    def print_full_name (self):
+    def print_full_name(self):
         print(self.first_name, self.last_name)
