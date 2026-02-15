e0c23fa12e91b0984552462114d3560f5ec248f2
Dimon21523 <dimitrisapollon041@gmail.com>
2026-02-16 01:59:11 +0200

    fix: Add docstrings, types and fix linting errors

--- lesson_10/pages/cart_page.py
+++ lesson_10/pages/cart_page.py
@@ -4,27 +4,32 @@ from lesson_10.pages.base_page import BasePage
 
 
 class CartPage(BasePage):
+    """Класс страницы корзины и товаров."""
+
     def __init__(self, driver):
         super().__init__(driver)
         self.url = "https://www.saucedemo.com/inventory.html"
 
-    ADD_TO_CART_BACKPACK = (By.ID, "add-to-cart-sauce-labs-backpack")
-    ADD_TO_CART_BOLT_TSHIRT = (By.ID, "add-to-cart-sauce-labs-bolt-t-shirt")
+    # Локаторы
+    ADD_BACKPACK = (By.ID, "add-to-cart-sauce-labs-backpack")
+    ADD_TSHIRT = (By.ID, "add-to-cart-sauce-labs-bolt-t-shirt")
     CART_ICON = (By.CLASS_NAME, "shopping_cart_link")
     CART_ITEM_TITLE = (By.CLASS_NAME, "inventory_item_name")
 
     @allure.step("Добавление товара 'Backpack' в корзину")
     def add_backpack_to_cart(self) -> None:
-        self.driver.find_element(*self.ADD_TO_CART_BACKPACK).click()
-
-    @allure.step("Добавление товара 'Bolt T-Shirt' в корзину")
-    def add_bolt_tshirt_to_cart(self) -> None:
-        self.driver.find_element(*self.ADD_TO_CART_BOLT_TSHIRT).click()
+        """Добавляет рюкзак в корзину."""
+        self.driver.find_element(*self.ADD_BACKPACK).click()
 
     @allure.step("Переход в корзину через иконку")
     def click_cart_icon(self) -> None:
+        """Кликает по иконке корзины для перехода."""
         self.driver.find_element(*self.CART_ICON).click()
 
     @allure.step("Получение текста названия товара в корзине")
     def get_cart_item_text(self) -> str:
+        """
+        Получает название товара, находящегося в корзине.
+        :return: Название товара (str).
+        """
         return self.driver.find_element(*self.CART_ITEM_TITLE).text
